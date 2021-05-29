84bb15b6dc6a199761d90abbfb02f87213138541
chetagra <chetanagrawalstar1999@gmail.com>
2021-05-29 18:10:17 +0530

    typecast added and variables modified

---
+++ src/com/company/tCast.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class tCast {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+        float d =a;
+        System.out.println(d);
+
+        double w= 1.2;
+        int k= (int)w;
+        System.out.println(k);
+
+    }
+}
--- src/com/company/variables.java
+++ src/com/company/variables.java
@@ -4,10 +4,14 @@ public class variables {
     public static void main(String[] args) {
         int A = 2;
         System.out.println(A);
-       float b= 1.5f;
+        float b= 1.5f;
         System.out.println(b);
         char c = 'k';
         System.out.println(c);
+        boolean d = false;
+        System.out.println(d);
+        String e= "apple";
+        System.out.println(e);
     }
 
 }
