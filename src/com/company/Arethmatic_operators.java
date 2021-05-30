41ee0a684bbf598096de8dbc22fe20e82a77d96e
chetagra <chetanagrawalstar1999@gmail.com>
2021-05-30 18:04:55 +0530

    operators

---
+++ src/com/company/Arethmatic_operators.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Arethmatic_operators {
+    public static void main(String[] args) {
+        double a=8.25;
+        double b=7.06;
+
+        double c= a%b;
+        System.out.println(c);
+
+        double d=a+b;
+        System.out.println(d);
+
+        double e=a*b;
+        System.out.println(e);
+    }
+}
