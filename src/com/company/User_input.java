775806468d2de8853806c877ee7cb6f4ffca80f5
chetagra <chetanagrawalstar1999@gmail.com>
2021-06-06 17:55:34 +0530

    lec5 n lec4

--- src/com/company/User_input.java
+++ src/com/company/User_input.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class User_input {
     public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
-        int l =s.nextInt();
-        int b= s.nextInt();
-        System.out.println();
+       Scanner s = new Scanner(System.in);
+       int a= s.nextInt();
+        System.out.println(a);
 
-        char b= s.next().charAt(0);
+        char ch= s.next().charAt(0);
+        System.out.println(ch);
+
+        String str= s.next();
+        System.out.println(str);
+
+        boolean b= s.nextBoolean();
         System.out.println(b);
 
-
+        double d= s.nextDouble();
+        System.out.println(d);
     }
 }
---
+++ src/com/company/area_rect.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class area_rect {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter length:");
+        int l = s.nextInt();
+        System.out.println("Enter Breadth:");
+        int b= s.nextInt();
+       int area=rect_area(l,b);
+        System.out.println("Your Computed Area is:"+" "+area);
+    }
+    public  static  int rect_area(int l,int b){
+        int area= l*b;
+        return  area;
+    }
+}
